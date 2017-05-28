717b0182d7dbf4472c0f2208edc3a70b87042905
erin.wallaard@outlook.com <erin.wallaard@outlook.com>
2017-05-28 13:05:48 +0200

    dingen veranderd

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.all('*', function(request, response) {
-    response.status(404);
-    response.send('404 - Not found');
-})
-
-
 app.get('/', function(request, response) {
     response.send('Hello Avans!');
 })
@@ -21,6 +15,11 @@ app.post('/', function(request, response) {
 
 app.put('/', function(request, response) {
     response.send('Hello Avans, PUT request received!');
+})
+
+app.get('*', function(request, response) {
+    response.status(404);
+    response.send('404 - Not found');
 })
 
 app.listen((process.env.PORT || 3000), function() {
