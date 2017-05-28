e0c61e588110617076bfa8937d5a45d4cd83312c
erin.wallaard@outlook.com <erin.wallaard@outlook.com>
2017-05-28 13:18:37 +0200

    veranderingen

--- server.js
+++ server.js
@@ -18,11 +18,6 @@ app.put('/', function(request, response) {
     response.send('Hello Avans, PUT request received!');
 })
 
-app.get('*', function(request, response) {
-    response.status(404);
-    response.send('404 - Not found');
-})
-
 app.get('/json', function(request, response) {
     response.json({
         'some_name': 'Value',
@@ -42,6 +37,11 @@ app.get('/json', function(request, response) {
             "twee", "drie", "vijf", "zeven"
         ]
     })
+})
+
+app.get('*', function(request, response) {
+    response.status(404);
+    response.send('404 - Not found');
 })
 
 app.listen((port), function() {
