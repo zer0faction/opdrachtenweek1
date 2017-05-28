1e00befec09176862ef1022ec86b993aea5610df
erin.wallaard@outlook.com <erin.wallaard@outlook.com>
2017-05-28 13:13:39 +0200

    dingen veranderd

--- server.js
+++ server.js
@@ -22,6 +22,27 @@ app.get('*', function(request, response) {
     response.send('404 - Not found');
 })
 
+app.get('/json', function(request, response) {
+    response.json({
+        'some_name': 'Value',
+        'an_array_of_objects': [
+            {
+                'another_name': 'Another value',
+                'a_further_name': "A further value"
+            },
+            {'yet_another_name': 'Yet another value'}
+        ],
+        'some_boolean': true,
+        'some_integer': 42,
+        'array_of_ints': [
+            2, 3, 5, 7, 11, 13
+        ],
+        'array_of_strings': [
+            "twee", "drie", "vijf", "zeven"
+        ]
+    })
+})
+
 app.listen((process.env.PORT || 3000), function() {
-    console.log('Server app is listening on port 3000');
+    console.log('Server app is listening on port'+port);
 })
